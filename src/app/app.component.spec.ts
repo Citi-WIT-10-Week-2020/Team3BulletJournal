7cb5859d37857d63f165e76a523074e571a3473f
ejdog32 <40522897+ejdog32@users.noreply.github.com>
2020-06-17 15:49:18 -0400

    Added the login feature with fake backend

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,11 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'InternshipProject'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('InternshipProject');
-  });
+  
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
