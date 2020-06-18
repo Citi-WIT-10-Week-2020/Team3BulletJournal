8c768882a210df0624bef812e6d69d1522405bfb
ejdog32 <40522897+ejdog32@users.noreply.github.com>
2020-06-18 09:51:05 -0400

    Adding CLI

--- karma.conf.js
+++ karma.conf.js
@@ -10,14 +10,13 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-     
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/InternshipProject'),
+      dir: require('path').join(__dirname, './coverage/BulletJournal'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
