49f1c59fdc6c6bae9a56772ca648398d98a7a34e
s414-3502 <58474320+s414-3502@users.noreply.github.com>
2020-07-09 17:37:26 -0400

    random friends drop-down working

--- src/app/coffee-chat-random-friends/coffee-chat-random-friends.component.ts
+++ src/app/coffee-chat-random-friends/coffee-chat-random-friends.component.ts
@@ -7,39 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoffeeChatRandomFriendsComponent implements OnInit {
 
-  peopleList = [];
-  dropdownSettings= {};
+  numberList = ["1", "2", "3", "4", "5"];
   
   ngOnInit(): void {
 
-    this.peopleList = [ //can add database community here later!!! and enforce max 5 people per chat
-      { person_id: 1, person_name: 'Emily'},
-      { person_id: 2, person_name: 'Shannon'},
-      { person_id: 3, person_name: 'Katie'},
-      { person_id: 4, person_name: 'Millie'},
-      { person_id: 5, person_name: 'Jordan'},
-      { person_id: 6, person_name: 'Michelle'},
-      { person_id: 7, person_name: 'Duy'},
-    ];
-
-    this.dropdownSettings = {
-      singleSelection: false,
-      idField: 'person_id',
-      textField: 'person_name',
-      selectAllText: 'Select All',
-      UnselectAllText: 'UnSelect All',
-      itemsShowLimit: 5,
-      allowSearchFilter: false
-    };
-
   }
-
-  onPersonSelect(person: any){
-    console.log(person);
-  }
-
-  onSelectAll(persons: any){
-    console.log(persons);
-  }
-
 }
