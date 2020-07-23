2d14accd3a4685f91cebbb760a4828e5d58ea48a
ejdog32 <40522897+ejdog32@users.noreply.github.com>
2020-07-23 14:28:35 -0400

    Trying to fix div errors

--- src/app/coffee-chat-select-friends/coffee-chat-select-friends.component.ts
+++ src/app/coffee-chat-select-friends/coffee-chat-select-friends.component.ts
@@ -1,90 +1,45 @@
 import { Component, OnInit} from '@angular/core';
-import { Router } from '@angular/router';
-import { UserService, AlertService, AuthenticationService } from '../_services';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-coffee-chat-select-friends',
   templateUrl: './coffee-chat-select-friends.component.html',
   styleUrls: ['./coffee-chat-select-friends.component.css'],
 })
-​
 export class CoffeeChatSelectFriendsComponent implements OnInit {
-  currentUser: any;
   peopleList = [];
-  createMeeting: FormGroup;
-  submitted = false;
-  loading = false;
-  selectedPeople = [];
-  maxCapacity = false;
+  dropdownSettings= {};
 
-  host = this.currentUser.username;
-  
-  constructor(
-    private router: Router,
-    private authenticationService: AuthenticationService,
-    private formBuilder: FormBuilder,
-    private userService: UserService,
-    private alertService: AlertService) 
-  {
-    this.currentUser = this.authenticationService.currentUserValue[0];
-   }
-​
-   ngOnInit(){
-    this.peopleList = this.currentUser.friends;
-    this.createMeeting = this.formBuilder.group({
+  ngOnInit(): void {
 
-      date: ['', Validators.required],
-      time: ['', Validators.required],
-    })
+    this.peopleList = [ //can add database community here later!!! and enforce max 5 people per chat
+      { person_id: 1, person_name: 'Emily'},
+      { person_id: 2, person_name: 'Shannon'},
+      { person_id: 3, person_name: 'Katie'},
+      { person_id: 4, person_name: 'Millie'},
+      { person_id: 5, person_name: 'Jordan'},
+      { person_id: 6, person_name: 'Michelle'},
+      { person_id: 7, person_name: 'Duy'},
+    ];
+
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'person_id',
+      textField: 'person_name',
+      selectAllText: 'Select All',
+      UnselectAllText: 'UnSelect All',
+      itemsShowLimit: 5,
+      allowSearchFilter: false
+    };
+
   }
 
-
-  userSelect(person){
-    if(this.selectedPeople.includes(person.username)){ //deselect
-      this.selectedPeople = this.selectedPeople.filter(p => p !== person.username)
-    }
-    else{
-      this.selectedPeople.push(person);
-    }
-
-    if(this.selectedPeople.length >= 5){
-      console.log(this.maxCapacity);
-      this.maxCapacity = true;
-    }
-    else{
-      this.maxCapacity = false;
-    }
-    console.log(this.selectedPeople);
-    console.log(this.maxCapacity);
+  onPersonSelect(person: any){
+    console.log(person);
   }
 
-  // convenience getter for easy access to form fields
-  get f() { return this.createMeeting.controls; }
-  
-  onSubmit() {
+  onSelectAll(persons: any){
+    console.log(persons);
+  }
 
-    this.submitted = true;
-    console.log("in on SUBMIT")
-    // reset alerts on submit
-    this.alertService.clear();
-    // stop here if form is invalid
-    if (this.createMeeting.invalid) {
-        return;
-    }
-​
-    this.loading = true;
-    this.authenticationService.createMeeting(this.currentUser.username, this.selectedPeople, this.f.date.value.substring(8,10), this.f.date.value.substring(5,7), this.f.date.value.substring(0,4), this.f.time.value, this.host)
-        .subscribe(
-            data => {
-                this.alertService.success('Meeting Scheduled', true);
-                this.router.navigate(['/coffee-chat-profiles'], { queryParams: { scheduled: true }});
-            },
-            error => {
-                this.alertService.error(error);
-                this.loading = false;
-            });
-
-            console.log("got through");
-}
 }
