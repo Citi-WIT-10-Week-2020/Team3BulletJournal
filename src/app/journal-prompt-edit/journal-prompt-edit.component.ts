bded21cc82dec66df17034d518c1dfdf690569df
Maanasi Vadlamani <maanasivadlamani@gmail.com>
2020-07-30 10:06:15 -0400

    autoload removal from edit journal

--- src/app/journal-prompt-edit/journal-prompt-edit.component.ts
+++ src/app/journal-prompt-edit/journal-prompt-edit.component.ts
@@ -58,12 +58,7 @@ export class JournalPromptEditComponent implements OnInit {
     this.title = this.prompts[Math.round(promptIndexSelected)];
   }
   ngOnInit(): void {
-    if (!localStorage.getItem('autoLoad')) { 
-      localStorage.setItem('autoLoad', 'no reload') 
-      location.reload() 
-    } else {
-      localStorage.removeItem('autoLoad') 
-    }
+   
     var d = new Date();
     var promptIndexSelected = Math.random() * (this.prompts.length - 1);
     var date = d.getUTCDate();
